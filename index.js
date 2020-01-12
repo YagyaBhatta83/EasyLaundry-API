7f74d71b67a3fdee288781f40a3362419acd4a30
YagyaBhatta83 <bhattayagya123@gmail.com>
2020-01-12 11:29:38 +0545

    express package installed and tested in localhost.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const name = 'hello'
-console.log(name);
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
