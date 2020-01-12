6a92affea7ad9e5a003280c1faef00d4f846c07f
YagyaBhatta83 <bhattayagya123@gmail.com>
2020-01-12 13:02:43 +0545

    successfully connected to mongodb.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config()
+
+const app = express();
+app.use(express.json());
+
+
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then((db) => {
+        console.log("Successfully connected to MongodB server");
+    }, (err) => console.log(err));
+
+  
+
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is running at localhost:${process.env.PORT}`);
+    });
