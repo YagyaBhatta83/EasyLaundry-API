814484a83ea32b7111b44ff2c9e9af81e1419f12
YagyaBhatta83 <bhattayagya123@gmail.com>
2020-01-23 08:48:47 +0545

    get item successfully and some changes in other file.

--- models/item.js
+++ models/item.js
@@ -1,15 +1,19 @@
-const mongoose = require("mongoose");
-const itemSchema = new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: true
-      },
-      price:{
-          type:String,
-          required: true
-      },
-    },
-      { timestamps: true }
-      );
-      module.exports = mongoose.model("Item", itemSchema);
+const mongoose=require('mongoose');
+
+const itemSchema=new mongoose.Schema({
+name:{
+type:String,
+required:true
+},
+price:{
+  type:Number,
+  required:true
+},service: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Service'
+}
+},{ timestamps: true });
+
+module.exports=mongoose.model("Item",itemSchema)
+
+
--- routes/item.js
+++ routes/item.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const Item = require("../models/item");
 const router = express.Router();
+const auth = require('../auth');
 
-router.post("/items", (req, res, next) => {
+router.post("/items", (req, res, err) => {
 
-      Item.create({
-        name: req.body.name,
-        price:req.body.price
-      })
-        .then(item => {
-          res.send( item );
-        })
-        .catch(next);
+  Item.create(req.body)
+  .then((item) => {
+      res.statusCode = 201;
+      res.json(item);
+  })
+        .catch(err => {
+            res.status(500).send(err)
+        });
     });
+
+router.get('/items',(req,res)=>{
+  Item.find({},(error,data)=>{
+    res.json(data);
+  })
+
+
+})
+
     module.exports = router;
 
