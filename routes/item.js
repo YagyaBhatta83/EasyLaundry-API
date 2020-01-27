b6a52275b8c99c9c6a04461c4e40e7b94808e4e1
YagyaBhatta83 <bhattayagya123@gmail.com>
2020-01-27 11:29:54 +0545

    upload image in item

--- routes/item.js
+++ routes/item.js
@@ -2,10 +2,38 @@ const express = require("express");
 const Item = require("../models/item");
 const router = express.Router();
 const auth = require('../auth');
+const multer = require('multer');
+const path = require("path");
 
-router.post("/items", (req, res, err) => {
+const storage = multer.diskStorage({
+    destination: "./public/uploads",
+    filename: (req, file, callback) => {
+        let ext = path.extname(file.originalname);
+        callback(null, `${file.fieldname}-${Date.now()}${ext}`);
+    }
+});
 
-  Item.create(req.body)
+const imageFileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error("You can upload only image files!"), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: imageFileFilter
+})
+
+router.post("/items",upload.single('image'), (req, res, err) => {
+
+  Item.create(
+      {
+          name:req.body.name,
+          price:req.body.price,
+          image:req.file.path
+      }
+  )
   .then((item) => {
       res.statusCode = 201;
       res.json(item);
